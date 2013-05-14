4f8a33c8471b17019388660c710365c6d02f9ac9
ndousse <ndousse@git.epfl.ch/repo/>
2013-05-14 17:20:45 +0200

    Merge branch 'maveric_cpp' of https://git.epfl.ch/repo/maveric into Nicolas
    Conflicts:
    	Code/Maveric/src/hal/servo_pwm.c
    	Code/Maveric/src/hal/servo_pwm.h
    	Code/Maveric/src/hal/uart_int.c
    	Code/MegaFly2.atsuo
    	Code/MegaFly2/Debug/Makefile
    	Code/MegaFly2/Debug/makedep.mk
    	Code/MegaFly2/MegaFly2.cproj
    	Code/MegaFly2/src/communication/ishtar_stream.c
    	Code/MegaFly2/src/communication/mavlink_stream.c
    	Code/MegaFly2/src/communication/mavlink_stream.h
    	Code/MegaFly2/src/communication/onboard_parameters.c
    	Code/MegaFly2/src/communication/onboard_parameters.h
    	Code/MegaFly2/src/config/conf_platform.h
    	Code/MegaFly2/src/config/conf_usart_serial.h
    	Code/MegaFly2/src/control/stabilisation.c
    	Code/MegaFly2/src/control/stabilisation.h
    	Code/MegaFly2/src/main.c
    	Code/MegaFly2/src/runtime/scheduler.c
    	Code/MegaFly2/src/runtime/scheduler.h
    	Code/MegaFly2/src/sensing/imu.c
    	Code/MegaFly2/src/sensing/qfilter.h
    	Code/MegaFly2/src/util/buffer.c
    	Code/MegaFly2/src/util/buffer.h
    	Code/MegaFly2/src/util/coord_conventions.c
    	Code/MegaFly2/src/util/print_util.c
    	Code/MegaFly2/src/util/print_util.h

---
+++ Code/src/control/estimator.c
@@ -0,0 +1,368 @@
+/*
+ * estimator.c
+ *
+ *  Created on: April 30, 2013
+ *      Author: Philippe
+ * ACCELERATION EN G!!
+ */
+
+#include "imu.h"
+#include "boardsupport.h"
+#include "gps_ublox.h"
+#include "bmp085.h"
+#include "estimator.h"
+#include "qfilter.h"
+
+#define INIT_X_P	10.
+#define INIT_Y_P	10.
+#define INIT_Z_P	10.
+
+#define Q_X0	0.001
+#define Q_X1	0.001
+#define Q_X2	0.00001
+#define Q_Y0	0.001
+#define Q_Y1	0.001
+#define Q_Y2	0.00001
+#define Q_Z0	0.001
+#define Q_Z1	0.001
+#define Q_Z2	0.00001
+
+#define R_X		2.
+#define R_Y		2.
+#define R_Z		2.
+
+#define COS_PI_4 0.7071 // cos(pi/4), for taylor approx for latitude of around 45°
+#define EARTH_RADIUS 0.6731 //in E-7 meter
+#define DEGREE_TO_RADIAN 0.0175 //pi/180
+#define PI_4 0.7854  //pi/4
+
+//Inverse quaternion
+#define QI(q, out) \ 
+	out.s = q.s;\
+	out.v[0] = -q.v[0];\
+	out.v[1] = -q.v[1];\
+	out.v[2] = -q.v[2];
+
+//Cross product
+#define CP(u,v,out)\ 
+	out[0]=u[1]*v[2]-u[2]*v[1];\
+	out[1]=u[2]*v[0]-u[0]*v[2];\
+	out[2]=u[0]*v[1]-u[1]*v[0];
+	
+#define MUL_V_SCA(u,a)\
+	u[0]=u[0]*a;\
+	u[1]=u[1]*a;\
+	u[2]=u[2]*a;
+
+//Rotation of vector v with the quaternion quat
+#define ROT_Q(quat,v,out)\ 
+	CP(quat.v,v,temp1)\
+	temp1[0]=temp1[0]+quat.s* *v[0];\
+	temp1[1]=temp1[1]+quat.s* *v[1];\
+	temp1[2]=temp1[2]+quat.s* *v[2];\
+	CP(quat.v,temp1,temp2)\
+	out[0]= *v[0]+2*temp2[0];\
+	out[1]= *v[1]+2*temp2[1];\
+	out[2]= *v[2]+2*temp2[2];
+	
+
+pressure_data *baro;
+board_hardware_t *board;
+Estimator_Data_t *estimator_;
+float P[3][3][3]; // Covariance matrice for Z,X and Y
+float Q[3][3];
+float R[3];
+long init_lat,init_long;
+float init_alt;
+float previous_P[3][3][3]; //Remember the state and covariance 200ms ago (so 10 predictions ago) to apply propagation
+float previous_state[3][3];//for delayed GPS measure (so we can get a max 200ms delay)
+float previous_acc[3][10];// We must stock the acc, dt and quaternion so that we can do the predictions until actual time
+float previous_dt[3][10];
+
+
+
+//----------------------------INITIALISATION------------------------
+void e_init()
+{
+	board=get_board_hardware();
+	while(!newValidGpsMsg())
+	{
+	e_kalman_init(X,INIT_X_P); //e stands for estimator not extended
+	e_kalman_init(Y,INIT_Y_P);
+	e_kalman_init(Z,INIT_Z_P);
+	}	
+	Q[0][0]=Q_X0;
+	Q[0][1]=Q_X1;
+	Q[0][2]=Q_X2;
+	Q[1][0]=Q_Y0;
+	Q[1][1]=Q_Y1;
+	Q[1][2]=Q_Y2;
+	Q[2][0]=Q_Z0;
+	Q[2][1]=Q_Z1;
+	Q[2][2]=Q_Z2;
+	R[0]=R_X;
+	R[1]=R_Y;
+	R[2]=R_Z;
+}
+
+void e_kalman_init (int axis,float init_p) // axis = Z, X or Y
+{
+	int i, j;
+	
+	if (axis==X)
+		init_long=board->GPS_data.longitude;	
+	else if (axis==Y)
+		init_lat=board->GPS_data.latitude;
+	else if (axis==Z)
+		init_alt=board->GPS_data.altitude;
+	
+	estimator_->state[axis][POSITION] = 0; // Differential par rapport au point de départ
+	estimator_->state[axis][SPEED] = 0; // Differential par rapport au point de départ
+	
+	if (axis==X)
+		estimator_->state[axis][BIAIS] = board->imu1.raw_bias[3];
+	else if (axis==Y)
+		estimator_->state[axis][BIAIS] = board->imu1.raw_bias[4];
+	else if (axis==Z)
+		estimator_->state[axis][BIAIS] = board->imu1.raw_bias[5];
+		
+	for (i=0; i<3; i++) {
+		for (j=0; j<3; j++)
+		P[axis][i][j] = 0.;
+		P[axis][i][i] = init_p;
+	}
+}
+
+//------------------------------PREDICTION--------------------------
+void e_predict (UQuat_t *qe, float *a, float dt)
+{
+	
+	UQuat_t inv_qe,q_temp;
+	float x[3],y[3],z[3],x_quad[3],y_quad[3],z_quad[3];
+	float temp1[3], temp2[3];
+	// Calculation of acceleration on x,y,z in NED 
+	x[0]=1;x[1]=0;x[2]=0; //definition of x,y,z in NED
+	y[0]=0;y[1]=1;y[2]=0;
+	z[0]=0;z[1]=0;z[2]=1;
+/*	ROT_Q(*qe,&x[0],x_quad) // get the x vector of the quad in NED
+	ROT_Q(*qe,&y[1],y_quad)
+	ROT_Q(*qe,&z[2],z_quad)
+	MUL_V_SCA(x_quad,a[0]) // get the right norm
+	MUL_V_SCA(y_quad,a[1])
+	MUL_V_SCA(z_quad,a[2])*/
+
+	e_kalman_predict(X,(x_quad[0]*x_quad[0]+y_quad[0]*y_quad[0]+z_quad[0]*z_quad[0]),dt);//final x (in NED) acc 
+	e_kalman_predict(Y,(x_quad[1]*x_quad[1]+y_quad[1]*y_quad[1]+z_quad[1]*z_quad[1]),dt);
+	e_kalman_predict(Z,(x_quad[2]*x_quad[2]+y_quad[2]*y_quad[2]+z_quad[2]*z_quad[2]),dt);
+}
+
+
+
+/*
+
+ state vector : X = [ x x_speed x_biais ]; 
+
+ F = [ 1 dt -dt^2/2
+       0  1 -dt
+       0  0   1     ];
+
+ B = [ dt^2/2 dt 0]';
+
+ Q = [ 0.01  0     0
+       0     0.01  0
+       0     0     0.001 ];
+
+ Xk1 = F * Xk0 + B * accel;
+
+ Pk1 = F * Pk0 * F' + Q;
+
+*/
+void e_kalman_predict (int axis,float accel_meas, float dt)
+{
+  float accel_corr;
+  float FPF00,FPF01,FPF02,FPF10,FPF11,FPF12,FPF20,FPF21,FPF22;
+  /* update state */
+  
+  if (axis == Z)
+	accel_corr = accel_meas - 1; // 1 for gravity
+  else
+	accel_corr = accel_meas;
+	
+  estimator_->state[axis][POSITION] = estimator_->state[axis][POSITION] + dt * estimator_->state[axis][SPEED]; // not exactly the function F defined above
+  estimator_->state[axis][SPEED] = estimator_->state[axis][SPEED] + dt * ( accel_corr - estimator_->state[axis][BIAIS]);
+  /* update covariance */
+  // F*P*F' calculation
+  FPF00 = P[axis][0][0] + dt * ( P[axis][1][0] + P[axis][0][1] + dt * P[axis][1][1] );
+  FPF01 = P[axis][0][1] + dt * ( P[axis][1][1] - P[axis][0][2] - dt * P[axis][1][2] );
+  FPF02 = P[axis][0][2] + dt * ( P[axis][1][2] );
+  FPF10 = P[axis][1][0] + dt * (-P[axis][2][0] + P[axis][1][1] - dt * P[axis][2][1] );
+  FPF11 = P[axis][1][1] + dt * (-P[axis][2][1] - P[axis][1][2] + dt * P[axis][2][2] );
+  FPF12 = P[axis][1][2] + dt * (-P[axis][2][2] );
+  FPF20 = P[axis][2][0] + dt * ( P[axis][2][1] );
+  FPF21 = P[axis][2][1] + dt * (-P[axis][2][2] );
+  FPF22 = P[axis][2][2];
+  // P = F*P*F' + Q
+  P[axis][0][0] = FPF00 + Q[axis][POSITION];
+  P[axis][0][1] = FPF01;
+  P[axis][0][2] = FPF02;
+  P[axis][1][0] = FPF10;
+  P[axis][1][1] = FPF11 + Q[axis][SPEED];
+  P[axis][1][2] = FPF12;
+  P[axis][2][0] = FPF20;
+  P[axis][2][1] = FPF21;
+  P[axis][2][2] = FPF22 + Q[axis][BIAIS];	
+}
+
+
+//--------------------------------UPDATE----------------------------
+
+/*
+  H = [1 0 0];
+  R = 0.1;
+  // state residual
+  y = rangemeter - H * Xm;
+  // covariance residual
+  S = H*Pm*H' + R;
+  // kalman gain
+  K = Pm*H'*inv(S);
+  // update state
+  Xp = Xm + K*y;
+  // update covariance
+  Pp = Pm - K*H*Pm;
+*/
+void e_kalman_update_position (int axis,float position_meas, uint32_t dt) 
+{
+	  float y,S,K1,K2,K3; 
+	  float P11,P12,P13,P21,P22,P23,P31,P32,P33;
+
+	  y = position_meas - estimator_->state[axis][POSITION];
+	  S = P[axis][0][0] + R[axis];
+	  K1 = P[axis][0][0] * 1/S;
+	  K2 = P[axis][1][0] * 1/S;
+	  K3 = P[axis][2][0] * 1/S;
+
+	  estimator_->state[axis][POSITION]    = estimator_->state[axis][POSITION]    + K1 * y;
+	  estimator_->state[axis][SPEED] = estimator_->state[axis][SPEED] + K2 * y;
+	  estimator_->state[axis][BIAIS] = estimator_->state[axis][BIAIS] + K3 * y;
+
+	  P11 = (1. - K1) * P[axis][0][0];
+	  P12 = (1. - K1) * P[axis][0][1];
+	  P13 = (1. - K1) * P[axis][0][2];
+	  P21 = -K2 * P[axis][0][0] + P[axis][1][0];
+	  P22 = -K2 * P[axis][0][1] + P[axis][1][1];
+	  P23 = -K2 * P[axis][0][2] + P[axis][1][2];
+	  P31 = -K3 * P[axis][0][0] + P[axis][2][0];
+	  P32 = -K3 * P[axis][0][1] + P[axis][2][1];
+	  P33 = -K3 * P[axis][0][2] + P[axis][2][2];
+
+	  P[axis][0][0] = P11;
+	  P[axis][0][1] = P12;
+	  P[axis][0][2] = P13;
+	  P[axis][1][0] = P21;
+	  P[axis][1][1] = P22;
+	  P[axis][1][2] = P23;
+	  P[axis][2][0] = P31;
+	  P[axis][2][1] = P32;
+	  P[axis][2][2] = P33;
+}
+
+
+/*
+  H = [0 1 0];
+  R = 0.1;
+  // state residual
+  yd = vz - H * Xm;
+  // covariance residual
+  S = H*Pm*H' + R;
+  // kalman gain
+  K = Pm*H'*inv(S);
+  // update state
+  Xp = Xm + K*yd;
+  // update covariance
+  Pp = Pm - K*H*Pm;
+*/
+void e_kalman_update_speed(int axis,float speed_meas, uint32_t dt)
+{
+  
+	float yd,S,K1,K2,K3;
+  	float P11,P12,P13,P21,P22,P23,P31,P32,P33;
+  
+	yd = speed_meas - estimator_->state[axis][SPEED];
+	S = P[axis][1][1] + R[axis];
+	K1 = P[axis][0][1] * 1/S;
+	K2 = P[axis][1][1] * 1/S;
+	K3 = P[axis][2][1] * 1/S;
+
+	estimator_->state[axis][POSITION] = estimator_->state[axis][POSITION] + K1 * yd;
+	estimator_->state[axis][SPEED] = estimator_->state[axis][SPEED] + K2 * yd;
+	estimator_->state[axis][BIAIS] = estimator_->state[axis][BIAIS] + K3 * yd;
+
+	P11 = -K1 * P[axis][1][0] + P[axis][0][0];
+	P12 = -K1 * P[axis][1][1] + P[axis][0][1];
+	P13 = -K1 * P[axis][1][2] + P[axis][0][2];
+	P21 = (1. - K2) * P[axis][1][0];
+	P22 = (1. - K2) * P[axis][1][1];
+	P23 = (1. - K2) * P[axis][1][2];
+	P31 = -K3 * P[axis][1][0] + P[axis][2][0];
+	P32 = -K3 * P[axis][1][1] + P[axis][2][1];
+	P33 = -K3 * P[axis][1][2] + P[axis][2][2];
+
+  P[axis][0][0] = P11;
+  P[axis][0][1] = P12;
+  P[axis][0][2] = P13;
+  P[axis][1][0] = P21;
+  P[axis][1][1] = P22;
+  P[axis][1][2] = P23;
+  P[axis][2][0] = P31;
+  P[axis][2][1] = P32;
+  P[axis][2][2] = P33;
+
+}
+
+
+
+//--------------------------------GLOBAL--------------------------
+void estimator()
+{
+	float pos_x,pos_y,pos_z;
+	double	latitude_rad;
+	float time_before_baro;
+	
+	static uint8_t init=0;
+	//static uint32_t dt_baro,time_before_baro;
+
+	
+	if (init==0)
+	{
+		e_init(); 
+		time_before_baro=get_millis();
+		init=1;
+	}
+	e_predict(&(board->imu1.attitude.qe),board->imu1.attitude.a,board->imu1.dt);
+	
+	//Check new values from GPS/Baro, if yes, update
+	if (newValidGpsMsg())
+	{
+		//longitude latitude to x,y position
+		latitude_rad= ((double) (board->GPS_data.latitude-init_lat))*DEGREE_TO_RADIAN; //in rad E+7
+		pos_x= (float) (((double) (board->GPS_data.longitude-init_long)*EARTH_RADIUS)*DEGREE_TO_RADIAN*(COS_PI_4-COS_PI_4*(latitude_rad*0.0000001-PI_4)-COS_PI_4*0.5*(latitude_rad*0.0000001-PI_4)*(latitude_rad*0.0000001-PI_4)));//Taylor 2nd order cos() approx
+		pos_y= (float) (latitude_rad*EARTH_RADIUS); //is altitude given from the center of the earth??-------------------------------------#########
+		pos_z= board->GPS_data.altitude-init_alt;
+		//get delay of GPS measure
+		//do prediction up to the corresponding delay
+		e_kalman_update_position(X,pos_x,(uint32_t) board->GPS_data.timeLastMsg);
+		e_kalman_update_position(Y,pos_y,(uint32_t) board->GPS_data.timeLastMsg);
+		e_kalman_update_position(Z,pos_z,(uint32_t) board->GPS_data.timeLastMsg);
+		e_kalman_update_speed(X,board->GPS_data.northSpeed,(uint32_t) board->GPS_data.timeLastMsg); 
+		e_kalman_update_speed(Y,board->GPS_data.eastSpeed,(uint32_t) board->GPS_data.timeLastMsg);
+		e_kalman_update_speed(Z,board->GPS_data.verticalSpeed,(uint32_t) board->GPS_data.timeLastMsg); 
+		//Continue the prediction until actual time
+	}	
+/*	if (newBaroValue()) // CHECK ------##############################3
+	{
+		dt_baro=get_millis()-time_before_baro;
+		baro=get_pressure_data_slow();
+		e_kalman_update_position(Z,baro->altitude,dt_baro);
+		time_before_baro=get_millis();
+	}	*/
+}
