4311eb326b0446583c8932f8ad7eb9ee996db0be
Julien Lecoeur <julien.lecoeur@epfl.ch>
2013-11-13 17:48:35 +0100

    Add missing cprog file

---
+++ Code/Maveric_myCopter/src/config/conf_stabilisation.h
@@ -0,0 +1,16 @@
+/*
+ * conf_stabilisation.h
+ *
+ * Default values for stabilisation
+ * 
+ * Created: 13/11/2013 17:46:00
+ *  Author: Julien
+ */
+
+#ifndef CONF_STABILISATION_H_
+#define CONF_STABILISATION_H_
+
+
+
+
+#endif /* CONF_STABILISATION_H_ */
