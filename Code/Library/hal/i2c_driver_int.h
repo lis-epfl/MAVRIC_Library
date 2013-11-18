9f12612e054a2df56b6103986c6632cce10df3a8
Felix Schill <felix.schill@epfl.ch>
2013-11-18 11:34:30 +0100

    added structure for I2C queuing system.

--- Code/Library/hal/i2c_driver_int.h
+++ Code/Library/hal/i2c_driver_int.h
@@ -12,6 +12,7 @@
 #include "twim.h"
 #include "compiler.h"
 #include "dma_channel_config.h"
+#include "scheduler.h"
 
 #define I2C_DEVICES 2
 #define I2C_SCHEDULE_SLOTS 10
@@ -21,26 +22,29 @@
 #define I2C_WRITE1_THEN_READ 2
 
 typedef struct {
-	unsigned char	slave_address; // I2C address of slave
+	uint8_t			slave_address; // I2C address of slave
 	uint32_t		i2c_speed;     // speed of i2c bus clock in kHz/kbps. Normally 100-400
 	char			direction;
 	char			write_then_read_preamble;
-	unsigned char*  data;
-	unsigned        data_size;
-	unsigned		data_index;
+	uint8_t			data;
+	uint16_t        data_size;
+	uint16_t		data_index;
 	bool			transfer_in_progress;
+	task_handle_t	*event_handler;
 } i2c_packet_t;
-
-
-typedef void (i2c_callback_t)(i2c_packet_t* data);
 
 
 
 int init_i2c(unsigned char i2c_device);
 
-char i2c_reset(unsigned char i2c_device);
+int i2c_append_transfer(i2c_packet_t *request);
+int i2c_append_read_transfer(uint8_t slave_address, uint8_t *data, uint16_t size, task_handle_t *event_handler);
+int i2c_append_write_transfer(uint8_t slave_address, uint8_t *data, uint16_t size, task_handle_t *event_handler);
+int i2c_append_register_read_transfer(uint8_t slave_address, uint8_t register_address, uint8_t *data, uint16_t size, task_handle_t *event_handler);
 
+int i2c_clear_queue();
 
-
+bool i2c_is_ready();
+int i2c_get_queued_requests();
 
 #endif /* I2C_DRIVER_H_ */
