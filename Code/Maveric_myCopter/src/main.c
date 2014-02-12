529c21dd9ccc9e6c705facf07cb37c842f1747f9
Felix Schill <Felix.Schill@epfl.ch>
2014-02-12 18:27:18 +0100

    HIL simulation: added uprighting on the ground, which seems to fix stability problem at startup

--- Code/Maveric_myCopter/src/main.c
+++ Code/Maveric_myCopter/src/main.c
@@ -105,8 +105,9 @@ void main (void)
 	create_tasks();
 	
 	// turn on simulation mode: 1: simulation mode, 0: reality
-	//centralData->simulation_mode = 0;
-	
+	centralData->simulation_mode = 1;
+	init_simulation(&(centralData->sim_model),&(centralData->imu1.attitude));
+
 	// main loop
 	
 	while (1==1) {
