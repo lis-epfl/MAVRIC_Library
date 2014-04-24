d9af9813433cd44aad97bc771aa26018d29a9a9b
Geraud L'Eplattenier <geraud.leplattenier@epfl.ch>
2014-04-24 17:34:01 +0200

    Cleanup the Bootloader directory with the working bootloader (default bootloader) and the new userpage and the rest moved in folder Old. The DFU files and script is added to the library. Update of the documentation with the use of the bootloader and the userpage as well as the DFU-programmer installing and using procedure.

---
+++ Code/Library/DFU-programmer/dfu-programming.bat
@@ -0,0 +1,4 @@
+dfu-programmer at32uc3c1512 erase
+dfu-programmer at32uc3c1512 get
+dfu-programmer at32uc3c1512 flash Debug\MegaFly2.hex --suppress-bootloader-mem 
+dfu-programmer at32uc3c1512 reset
