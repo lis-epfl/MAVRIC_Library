615f5ecdff0738a49bef891d0e1f68308153fe2e
Gregoire Heitz <gregoire.heitz@gmail.com>
2014-06-24 09:40:31 +0200

    add config_imu Greg calib

---
+++ Code/Maveric_gumstix/src/config/Mavsettings/MAV008_conf_imu_rev4.h
@@ -0,0 +1,55 @@
+/*
+ * conf_imu_rev4.h
+ *
+ * Created: 20/11/2013 22:21:49
+ *  Author: sfx
+ */ 
+
+
+#ifndef CONF_IMU_REV4_H_
+#define CONF_IMU_REV4_H_
+
+
+#define RAW_GYRO_X 0
+#define RAW_GYRO_Y 1
+#define RAW_GYRO_Z 2
+
+#define RAW_ACC_X 0
+#define RAW_ACC_Y 1
+#define RAW_ACC_Z 2
+
+#define RAW_COMPASS_X 2
+#define RAW_COMPASS_Y 0
+#define RAW_COMPASS_Z 1
+
+// from datasheet: FS 2000dps --> 70 mdps/digit
+// scale = 1/(0.07 * PI / 180.0) = 818.5111
+#define RAW_GYRO_X_SCALE  1637.02223
+#define RAW_GYRO_Y_SCALE  -1637.02223
+#define RAW_GYRO_Z_SCALE  -1637.02223
+
+#define RAW_ACC_X_SCALE  3960.7
+#define RAW_ACC_Y_SCALE  4005.7
+#define RAW_ACC_Z_SCALE  4026.8
+
+#define ACC_BIAIS_X 64
+#define ACC_BIAIS_Y  128
+#define ACC_BIAIS_Z  210
+
+#define ACC_AXIS_X  1.0
+#define ACC_AXIS_Y -1.0
+#define ACC_AXIS_Z -1.0
+
+#define RAW_MAG_X_SCALE 624.01
+#define RAW_MAG_Y_SCALE 590.35
+#define RAW_MAG_Z_SCALE 523.8
+
+#define MAG_BIAIS_X  -50				
+#define MAG_BIAIS_Y -150
+#define MAG_BIAIS_Z   7.17
+
+#define MAG_AXIS_X -1.0
+#define MAG_AXIS_Y -1.0
+#define MAG_AXIS_Z  1.0
+
+#endif /* CONF_IMU_REV4_H_ */
---
+++ Code/Maveric_gumstix/src/config/Mavsettings/MAV203_conf_imu_rev4.h
@@ -0,0 +1,59 @@
+/*
+ * conf_imu_rev4.h
+ *
+ * Created: 20/11/2013 22:21:49
+ *  Author: sfx
+ */ 
+
+
+#ifndef CONF_IMU_REV4_H_
+#define CONF_IMU_REV4_H_
+
+
+#define RAW_GYRO_X 0
+#define RAW_GYRO_Y 1
+#define RAW_GYRO_Z 2
+
+#define RAW_ACC_X 0
+#define RAW_ACC_Y 1
+#define RAW_ACC_Z 2
+
+#define RAW_COMPASS_X 2
+#define RAW_COMPASS_Y 0
+#define RAW_COMPASS_Z 1
+
+// from datasheet: FS 2000dps --> 70 mdps/digit
+// scale = 1/(0.07 * PI / 180.0) = 818.5111
+#define RAW_GYRO_X_SCALE  818.5111
+#define RAW_GYRO_Y_SCALE  818.5111
+#define RAW_GYRO_Z_SCALE  818.5111
+
+#define GYRO_AXIS_X  1.0
+#define GYRO_AXIS_Y -1.0
+#define GYRO_AXIS_Z -1.0
+
+#define RAW_ACC_X_SCALE  4114.96
+#define RAW_ACC_Y_SCALE  4052.16
+#define RAW_ACC_Z_SCALE  4007.06
+
+#define ACC_BIAIS_X  85.00//-97.20;
+#define ACC_BIAIS_Y  250.00//124.35;
+#define ACC_BIAIS_Z  130//-40.73;
+
+#define ACC_AXIS_X  1.0
+#define ACC_AXIS_Y -1.0
+#define ACC_AXIS_Z -1.0
+
+#define RAW_MAG_X_SCALE 490.57//624.11
+#define RAW_MAG_Y_SCALE 501.79//590.77
+#define RAW_MAG_Z_SCALE 436.09//512.37
+
+#define MAG_BIAIS_X -315.89//-127.86
+#define MAG_BIAIS_Y 37.87//-138.32
+#define MAG_BIAIS_Z -518.49//-79.38
+
+#define MAG_AXIS_X -1.0
+#define MAG_AXIS_Y -1.0
+#define MAG_AXIS_Z -1.0
+
+#endif /* CONF_IMU_REV4_H_ */
