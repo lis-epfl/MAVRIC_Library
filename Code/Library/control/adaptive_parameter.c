063d47a22586034176cce919f4d39ab95fe53654
meysam <meysam.basiri@epfl.ch>
2014-07-01 17:03:14 +0200

    Added comments and Headers for files and functions in the control folder ORCA needs to be completed

--- Code/Library/control/adaptive_parameter.c
+++ Code/Library/control/adaptive_parameter.c
@@ -1,9 +1,38 @@
-/*
- * adaptive_parameter.c
+/**
+ * This file automatically adapts the existing parameters based on given control variable
  *
- * Created: 03/11/2013 16:44:00
- *  Author: julien
- */ 
+ * [usage example:
+ *	// control variable
+ *	float control = 0.0;
+ *	// param1
+ *	float param1 = 0.0;
+ *	int number_set_points1 = 5;
+ *	float new_setpoints1[5] = {0.1, 0.2, 0.4, 0.7, 0.9};
+ *	float new_setvalues1[5] = {0, 1, 0, 100, 0};
+ *	add_adaptive_parameter(&control, &param1, number_set_points1, new_setpoints1, new_setvalues1);
+ *	// param2
+ *	float param2 = 0.0;
+ *	int number_set_points2 = 2;
+ *	float new_setpoints2[2] = {0.1, 1.5};
+ *	float new_setvalues2[2] = {10, -10};
+ *	add_adaptive_parameter(&control, &param2, number_set_points2,
+ *							new_setpoints2, new_setvalues2);
+ *	printf("Control, Param1, Param2\n");
+ *	int i;
+ *	for (i = 0; i < 20; ++i)
+ *	{
+ *		control = 1 / 20.0 * i;
+ *		update_all_adaptive_parameters();
+ *		printf("%f, %f, %f\n", control, param1, param2);
+ *	}]
+ *
+ * The MAV'RIC Framework
+ * Copyright © 2011-2014
+ *
+ * Laboratory of Intelligent Systems, EPFL
+ *
+ * This file is part of the MAV'RIC Framework.
+ */
 
 #include "adaptive_parameter.h"
 #include "maths.h"
