fdb46f24bda1b29e4ed2c47bf7ee557859177e10
Géraud L'Eplattenier <geraud.leplattenier@epfl.ch>
2014-07-03 15:35:38 +0200

    Update comments and headers and reformate code, what else?

--- Code/Library/control/stabilisation_copter.h
+++ Code/Library/control/stabilisation_copter.h
@@ -1,21 +1,32 @@
-/**
- *  This file handles the stabilization of the platform
+/** 
+ * \page The MAV'RIC license
  *
  * The MAV'RIC Framework
+ *
  * Copyright © 2011-2014
  *
  * Laboratory of Intelligent Systems, EPFL
+ */
+ 
+ 
+/**
+ * \file stabilisation_copter.h
  *
- * This file is part of the MAV'RIC Framework.
+ * This file handles the stabilization of the platform
  */
 
 
 #ifndef STABILISATION_COPTER_H_
 #define STABILISATION_COPTER_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "stabilisation.h"
 
-typedef struct {
+typedef struct 
+{
 	Stabiliser_t rate_stabiliser;
 	Stabiliser_t attitude_stabiliser;
 	Stabiliser_t velocity_stabiliser;
@@ -23,35 +34,44 @@ typedef struct {
 } Stabiliser_Stack_copter_t;
 
 /**
- * \brief Initialize module stabilization
- * \param stabiliser_stack pointer to structure with all controllers 
+ * \brief						Initialize module stabilization
+ *
+ * \param	stabiliser_stack	Pointer to structure with all controllers 
  */
 void init_stabilisation_copter(Stabiliser_Stack_copter_t* stabiliser_stack);
 
 /**
- * \brief gets the velocity vector from the remote
- * \param tvel pointer to array for storing the velocity vector 
+ * \brief						Gets the velocity vector from the remote
+ *
+ * \param	tvel				Pointer to array for storing the velocity vector 
  */
 void get_velocity_vector_from_remote(float tvel[]);
 
 /**
- * \brief Main Controller for controlling and stabilizing the quad
- * \param imu Pointer to Imu
- * \param pos_est pointer to position estimator
- * \param control_input pointer to the controlling inputs
+ * \brief						Main Controller for controlling and stabilizing the quad
+ *
+ * \param	imu					Pointer to Imu
+ * \param	pos_est				Pointer to position estimator
+ * \param	control_input		Pointer to the controlling inputs
  */
 void cascade_stabilise_copter(Imu_Data_t *imu, position_estimator_t *pos_est, Control_Command_t *control_input);
 
 /**
- * \brief mix to servo for quad configuration diagonal
- * \param control pointer to controlling inputs
+ * \brief						Mix to servo for quad configuration diagonal
+ *
+ * \param	control				Pointer to controlling inputs
  */
 void mix_to_servos_diag_quad(Control_Command_t *control);
 
 /**
- * \brief mix to servo for quad configuration cross
- * \param control pointer to controlling inputs
+ * \brief						Mix to servo for quad configuration cross
+ *
+ * \param	control				Pointer to controlling inputs
  */
 void mix_to_servos_cross_quad(Control_Command_t *control);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* STABILISATION_COPTER_H_ */
