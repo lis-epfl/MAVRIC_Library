7e92cb77b4a2ed8109610cfed8454a0341c0c161
Géraud L'Eplattenier <geraud.leplattenier@epfl.ch>
2014-07-03 10:25:01 +0200

    Move DFU folder in the Library and update documentation for DFU programming git status

--- Code/Maveric_myCopter/dfu-programming.bat
+++ Code/Maveric_myCopter/dfu-programming.bat
@@ -1,5 +1,5 @@
-DFU\dfu-programmer at32uc3c1512 erase
-DFU\dfu-programmer at32uc3c1512 get
-DFU\dfu-programmer at32uc3c1512 flash .\Release\MegaFly2.hex --suppress-bootloader-mem 
-DFU\dfu-programmer at32uc3c1512 reset
+Library\DFU-programmer\DFU\dfu-programmer at32uc3c1512 erase
+Library\DFU-programmer\DFU\dfu-programmer at32uc3c1512 get
+Library\DFU-programmer\DFU\dfu-programmer at32uc3c1512 flash Release\MegaFly2.hex --suppress-bootloader-mem 
+Library\DFU-programmer\DFU\dfu-programmer at32uc3c1512 reset
 pause
