17bd534b508e1b92ce6dd9a07b992c938bdf2449
Gregoire Heitz <gregoire.heitz@gmail.com>
2014-07-07 14:19:39 +0200

    Add documentation + Code Cleaning for this file

--- Code/Library/hal/radar_driver.h
+++ Code/Library/hal/radar_driver.h
@@ -1,9 +1,19 @@
-/*
- * radar_driver.h
+/**
+ * \page The MAV'RIC License
  *
- * Created: 19/05/2012 00:29:41
- *  Author: sfx
- */ 
+ * The MAV'RIC Framework
+ *
+ * Copyright © 2011-2014
+ *
+ * Laboratory of Intelligent Systems, EPFL
+ */
+
+
+/**
+ * \file radar_driver.h
+ * 
+ * The radar driver
+ */
 
 
 #ifndef RADAR_DRIVER_H_
@@ -12,11 +22,20 @@
 #include "compiler.h"
 #include "gpio.h"
 
-#define RADAR_POWER1_PIN AVR32_PIN_PC17
-#define RADAR_POWER2_PIN AVR32_PIN_PC18
+#define RADAR_POWER1_PIN AVR32_PIN_PC17		///< Define the microcontroller pin mapped with the radar power pin1
+#define RADAR_POWER2_PIN AVR32_PIN_PC18		///< Define the microcontroller pin mapped with the radar power pin2
 
+/**
+ * \brief Initialize the radar driver
+ *\
 void radar_driver_init(void);
 
+/**
+ * \brief Switch the radar power to high or low
+ *
+ * \param supply1 set power pin1 to a high or low level
+ * \param supply2 set power pin2 to a high or low level
+ *\
 void radar_driver_switch_power(int supply1, int supply2);
 
 
