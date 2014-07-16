99dcb2c57bed6a7f7e8f800e4845ff6c356d9e00
Julien Lecoeur <julien.lecoeur@epfl.ch>
2014-07-16 15:26:41 +0200

    Update documentation in mavlink_communication.h

--- Code/Library/communication/mavlink_communication.h
+++ Code/Library/communication/mavlink_communication.h
@@ -28,26 +28,36 @@ extern "C" {
 #include "mavlink_message_handler.h"
 #include "onboard_parameters.h"
 
-// TODO: update documentation
 
-
+/**
+ * \brief 	Main Mavlink Communication structure
+ */
 typedef struct 
 {
-	task_set_t task_set;
-	mavlink_stream_t mavlink_stream;				
-	mavlink_message_handler_t message_handler;
-	onboard_parameters_t onboard_parameters;
+	task_set_t 						task_set;					///<	Task set for scheduling of down messages
+	mavlink_stream_t 				mavlink_stream;				///< 	Mavlink interface using streams
+	mavlink_message_handler_t 		message_handler;			///< 	Message handler
+	onboard_parameters_t 			onboard_parameters;			///< 	Onboard parameters
 } mavlink_communication_t;
 
 
+/**
+ * \brief 	Configuratioon of the module Mavlink Communication
+ */
 typedef struct
 {
-	mavlink_stream_conf_t 			mavlink_stream_config;
-	mavlink_message_handler_conf_t	message_handler_config;
-	onboard_parameters_conf_t		onboard_parameters_config;
+	mavlink_stream_conf_t 			mavlink_stream_config;		///< 	Configuration for the module mavlink stream
+	mavlink_message_handler_conf_t	message_handler_config;		///< 	Configuration for the module message handler
+	onboard_parameters_conf_t		onboard_parameters_config;	///< 	Configuration for the module onboard parameters
 } mavlink_communication_conf_t;
 
 
+/**
+ * \brief 	Initialisation of the module mavlink communication
+ * 
+ * \param 	mavlink_communication 	Pointer to the mavlink communication structure
+ * \param 	config 					Configuration
+ */
 void mavlink_communication_init(mavlink_communication_t* mavlink_communication, const mavlink_communication_conf_t* config);
 
 
