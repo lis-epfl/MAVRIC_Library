529601979dc312b2c5c9170f067dc8818682afcd
ndousse <nicolas.dousse@epfl.ch>
2014-07-16 10:47:18 +0200

    Adapted waypoint handler to remove dependency on central_data

--- Code/Maveric_myCopter/src/central_data.c
+++ Code/Maveric_myCopter/src/central_data.c
@@ -36,7 +36,7 @@ void central_data_init()
 	qfilter_init_quaternion((Quat_Attitude_t*)&(centralData.imu1.attitude));
 		
 	navigation_init();
-	waypoint_handler_init();// ((waypoint_handler_t*)&centralData.waypoint_handler);
+	waypoint_handler_init(&centralData.waypoint_handler,&centralData.position_estimator,&centralData.imu1.attitude,&centralData.state_structure,&centralData.mavlink_communication);// ((waypoint_handler_t*)&centralData.waypoint_handler);
 		
 	neighbors_selection_init((neighbor_t*)&(centralData.neighborData), (position_estimator_t*)&(centralData.position_estimator));
 	orca_init((orca_t*)&(centralData.orcaData),(neighbor_t*)&(centralData.neighborData),(position_estimator_t*)&(centralData.position_estimator),(Imu_Data_t*)&(centralData.imu1));
