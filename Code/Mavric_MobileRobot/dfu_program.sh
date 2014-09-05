7effd4b2229b2cc91b068761d9192d40f4ec878f
Géraud L'Eplattenier <geraud.leplattenier@epfl.ch>
2014-09-05 17:02:47 +0200

    Remove folder MobileRobots as it was moved in a separate repository

--- Code/Mavric_MobileRobot/dfu_program.sh
+++
@@ -1,4 +0,0 @@
-dfu-programmer at32uc3c1512 erase
-dfu-programmer at32uc3c1512 get 
-dfu-programmer at32uc3c1512 flash Debug_Linux/Mavric_MobRob_linux.hex --suppress-bootloader-mem 
-dfu-programmer at32uc3c1512 reset 
