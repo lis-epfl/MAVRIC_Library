8f935d3f922dc596d757e3a609512871654d9866
ndousse <nicolas.dousse@epfl.ch>
2014-10-08 15:17:29 +0200

    Remove the _new from the message remote_telemetry_send_scaled

--- communication/remote_telemetry.c
+++ communication/remote_telemetry.c
@@ -120,7 +120,7 @@ void remote_telemetry_send_raw(const remote_t* remote, const mavlink_stream_t* m
 										remote->signal_quality	);
 }
 
-void remote_telemetry_send_scaled_new(const remote_t* remote, const mavlink_stream_t* mavlink_stream, mavlink_message_t* msg)
+void remote_telemetry_send_scaled(const remote_t* remote, const mavlink_stream_t* mavlink_stream, mavlink_message_t* msg)
 {
 	mavlink_msg_rc_channels_scaled_pack(	mavlink_stream->sysid,
 											mavlink_stream->compid,
