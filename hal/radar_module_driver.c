9d3116455c18250bf86520ca849a664003235dea
Julien Lecoeur <julien.lecoeur@epfl.ch>
2015-01-20 18:14:53 +0100

    Cleaned mavric message set

--- hal/radar_module_driver.c
+++ hal/radar_module_driver.c
@@ -44,9 +44,6 @@
 #include "i2c_driver_int.h"
 #include "print_util.h"
 
-///< Declare an object containing the MAVLink radar tracked target structure
-mavlink_radar_tracked_target_t main_target;
-
 void radar_module_init() 
 {
 	static twim_options_t twi_opt= 
@@ -62,19 +59,14 @@ void radar_module_init()
 }
 
 
-void radar_module_read() 
+void radar_module_read(radar_target_t* main_target) 
 {
 	uint8_t output = 0;
 	//uint8_t input [8];
 	twim_write(&AVR32_TWIM1, (uint8_t*) &output, 1, 1, false);
-	twim_read(&AVR32_TWIM1, (uint8_t*)&main_target, sizeof(main_target), 1, false);
+	twim_read(&AVR32_TWIM1, (uint8_t*)main_target, sizeof(main_target), 1, false);
 	
-	print_util_dbg_print_num(main_target.velocity * 100.0f,10);
-	print_util_dbg_print_num(main_target.amplitude,10);
+	print_util_dbg_print_num(main_target->velocity * 100.0f,10);
+	print_util_dbg_print_num(main_target->amplitude,10);
 	print_util_dbg_print("\r\n");;
-}
-
-mavlink_radar_tracked_target_t* radar_module_get_main_target() 
-{
-	return &main_target;	
 }
