c1a3f09d2ed28a7ce53d959575b62a82b5f6e4f0
Julien Lecoeur <julien.lecoeur@epfl.ch>
2015-07-27 17:17:31 +0200

    Cosmetics

--- hal/linux/serial_udp.cpp
+++ hal/linux/serial_udp.cpp
@@ -40,23 +40,9 @@
 
 #include "serial_udp.hpp"
 
-
 extern "C"
 {
-	#include <unistd.h>
-	// #include <stdlib.h>
-	#include <stdio.h>
-	#include <errno.h>
-	#include <string.h>
-	#include <sys/socket.h>
-	#include <sys/types.h>
-	#include <netinet/in.h>
 	#include <fcntl.h>
-	#include <time.h>
-	#include <sys/time.h>
-	#include <time.h>
-	#include <arpa/inet.h>
-	// #include "print_util.h"
 }
 
 //------------------------------------------------------------------------------
