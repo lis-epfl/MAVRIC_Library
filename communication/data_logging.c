8c79ecab6ead1e8485afcbee5130e54c511735ad
Matthew <mdouglas90@gmail.com>
2015-10-22 10:47:12 +0200

    added ! to if statement

--- communication/data_logging.c
+++ communication/data_logging.c
@@ -603,8 +603,8 @@ bool data_logging_open_new_log_file(data_logging_t* data_logging)
 			// Add extension (.txt) to name_n_extension
 			data_logging_filename_append_extension(data_logging->name_n_extension, data_logging->name_n_extension);
 
-			// Check if there was enough memory allocated to name_n_extension
-			if (is_string_terminated(data_logging->name_n_extension, data_logging->buffer_name_size))
+			// Check if there wasn't enough memory allocated to name_n_extension
+			if (!is_string_terminated(data_logging->name_n_extension, data_logging->buffer_name_size))
 			{
 				print_util_dbg_print("Name error: The name is too long! It should be, with the extension, maximum ");
 				print_util_dbg_print_num(data_logging->buffer_name_size,10);
