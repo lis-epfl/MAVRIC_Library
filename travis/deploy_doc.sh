46f816eea7b388341d871f7a4e631ac43bbae58c
Julien Lecoeur <julien.lecoeur@epfl.ch>
2016-01-27 10:22:25 +0100

    Test 2

--- travis/deploy_doc.sh
+++ travis/deploy_doc.sh
@@ -3,6 +3,17 @@
 # save git hash
 GITHASH=$(git rev-parse HEAD)
 
+cd ../doc/gh_pages
 
+git init
+git config user.name "MavricBot"
+git config user.email "bot@mavric.org"
+git remote add upstream "https://$GH_TOKEN@github.com/lis-epfl/MAVRIC_Library.git"
+git fetch upstream && git reset upstream/gh-pages
 
-echo pwd
+touch .
+
+git add -A .
+
+git commit -m "Rebuild doc at ${GITHASH}"
+git push -q upstream HEAD:gh-pages
