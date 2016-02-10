b4cba6b6f77a22fb3d9a05839878d831528f25cc
Alexandre <alexandre.cherpillod@epfl.ch>
2016-02-10 13:11:31 +0100

    added MAVCalib

---
+++ sample_projects/LEQuad/proj_avr32/config/MAVcalib/MAV203_imu_config.hpp
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2014, MAV'RIC Development Team
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * \file MAV203_imu_config.h
+ * 
+ * \author MAV'RIC Team
+ *   
+ * \brief This file defines the mapping between the IMU and the compass and the 
+ * frames of the vehicles as well as the scales and the biaises. 
+ * The NED frame is used.
+ *
+ ******************************************************************************/
+
+
+#ifndef CONF_IMU_REV4_H_
+#define CONF_IMU_REV4_H_
+
+#include "imu.hpp"
+#include "megafly_rev4.hpp"
+
+static inline imu_conf_t imu_config(void)
+{
+	imu_conf_t conf	= megafly_rev4_default_config().imu_config;
+
+	// Accelerometer Bias
+	conf.accelerometer.bias[0] = -175.00f / 4000.0f;
+	conf.accelerometer.bias[1] =  130.00f / 4000.0f;
+	conf.accelerometer.bias[2] =  400.00f / 4000.0f;
+	
+	// Gyroscope Bias
+	conf.gyroscope.bias[0] =  31.9f / 818.5111f;
+	conf.gyroscope.bias[1] = -18.0f / 818.5111f;
+	conf.gyroscope.bias[2] =  -2.0f / 818.5111f;
+	
+	// Magnetometer Bias
+	conf.magnetometer.bias[0] = -315.89f / 600.0f;
+	conf.magnetometer.bias[1] =   37.87f / 600.0f;
+	conf.magnetometer.bias[2] = -518.49f / 600.0f;
+
+	return conf;
+}
+
+#endif /* CONF_IMU_REV4_H_ */
