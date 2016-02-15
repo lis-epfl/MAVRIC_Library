48e96f7eba63856515b3798fcab4d96955349b81
ndousse <nicolas.dousse@epfl.ch>
2016-02-15 11:44:32 +0100

    Creation of LEWing sample project

---
+++ sample_projects/LEWing/central_data.cpp
@@ -0,0 +1,332 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2016, MAV'RIC Development Team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * \file central_data.c
+ *
+ * \author MAV'RIC Team
+ *
+ * \brief Place where the central data is stored and initialized
+ *
+ ******************************************************************************/
+
+
+#include "sample_projects/LEQuad/central_data.hpp"
+#include "control/stabilisation_copter_default_config.hpp"
+#include "communication/toggle_logging_default_config.hpp"
+#include "communication/mavlink_communication_default_config.hpp"
+
+#include "sensing/position_estimation_default_config.hpp"
+#include "communication/remote_default_config.hpp"
+#include "control/manual_control_default_config.hpp"
+#include "communication/toggle_logging.hpp"
+#include "control/attitude_controller_default_config.h"
+#include "control/velocity_controller_copter_default_config.h"
+#include "control/servos_mix_quadcopter_diag_default_config.hpp"
+
+extern "C"
+{
+#include "hal/common/time_keeper.hpp"
+#include "control/navigation_default_config.h"
+#include "sensing/qfilter_default_config.h"
+#include "runtime/scheduler_default_config.h"
+
+#include "util/print_util.h"
+}
+
+
+Central_data::Central_data(uint8_t sysid, Imu& imu, Barometer& barometer, Gps& gps, Sonar& sonar, Serial& serial_mavlink, Satellite& satellite, Led& led, File& file_flash, Battery& battery, Servo& servo_0, Servo& servo_1, Servo& servo_2, Servo& servo_3, File& file1, File& file2):
+    imu(imu),
+    barometer(barometer),
+    gps(gps),
+    sonar(sonar),
+    serial_mavlink(serial_mavlink),
+    satellite(satellite),
+    led(led),
+    file_flash(file_flash),
+    battery(battery),
+    servo_0(servo_0),
+    servo_1(servo_1),
+    servo_2(servo_2),
+    servo_3(servo_3),
+    state(battery, state_default_config()),
+    data_logging(file1),
+    data_logging2(file2),
+    sysid_(sysid)
+{}
+
+
+bool Central_data::init(void)
+{
+    bool init_success = true;
+    bool ret;
+
+    print_util_dbg_sep('%');
+    time_keeper_delay_ms(50);
+    print_util_dbg_sep('-');
+    time_keeper_delay_ms(50);
+    print_util_dbg_print("[CENTRAL_DATA] ...\r\n");
+    time_keeper_delay_ms(50);
+    print_util_dbg_sep('-');
+
+
+    // -------------------------------------------------------------------------
+    // Init main sheduler
+    // -------------------------------------------------------------------------
+    ret = scheduler_init(&scheduler, scheduler_default_config());
+    print_util_dbg_init_msg("[SCHEDULER]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init mavlink communication
+    // -------------------------------------------------------------------------
+    mavlink_communication_conf_t mavlink_communication_config = mavlink_communication_default_config();
+    mavlink_communication_config.mavlink_stream_config.sysid = sysid_;
+    mavlink_communication_config.message_handler_config.debug = true;
+    mavlink_communication_config.onboard_parameters_config.debug = true;
+    mavlink_communication_config.mavlink_stream_config.debug = true;
+    ret = mavlink_communication_init(&mavlink_communication,
+                                     mavlink_communication_config,
+                                     &serial_mavlink,
+                                     &state,
+                                     &file_flash);
+    print_util_dbg_init_msg("[MAVLINK]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    //Init state_machine
+    // -------------------------------------------------------------------------
+    ret = state_machine_init(&state_machine,
+                             &state,
+                             &gps,
+                             &imu,
+                             &manual_control);
+    print_util_dbg_init_msg("[STATE MACHINE]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init ahrs
+    // -------------------------------------------------------------------------
+    ret = ahrs_init(&ahrs);
+    print_util_dbg_init_msg("[AHRS]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init qfilter
+    // -------------------------------------------------------------------------
+    ret = qfilter_init(&attitude_filter,
+                       qfilter_default_config(),
+                       &imu,
+                       &ahrs);
+    print_util_dbg_init_msg("[QFILTER]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init position_estimation_init
+    // -------------------------------------------------------------------------
+    ret = position_estimation_init(&position_estimation,
+                                   position_estimation_default_config(),
+                                   &state,
+                                   &barometer,
+                                   &sonar,
+                                   &gps,
+                                   &ahrs);
+    print_util_dbg_init_msg("[POS EST]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init navigation
+    // -------------------------------------------------------------------------
+    ret = navigation_init(&navigation,
+                          navigation_default_config(),
+                          &controls_nav,
+                          &ahrs.qe,
+                          &position_estimation,
+                          &state,
+                          &mavlink_communication);/*,
+                            &sonar_i2cxl);*/
+    print_util_dbg_init_msg("[NAV]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init waypoint handler
+    // -------------------------------------------------------------------------
+    ret = waypoint_handler_init(&waypoint_handler,
+                                &position_estimation,
+                                &navigation,
+                                &ahrs,
+                                &state,
+                                &manual_control,
+                                &mavlink_communication,
+                                &mavlink_communication.mavlink_stream);
+    waypoint_handler_init_homing_waypoint(&waypoint_handler);
+    waypoint_handler_nav_plan_init(&waypoint_handler);
+    print_util_dbg_init_msg("[WAYPOINT]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init stabilisers
+    // -------------------------------------------------------------------------
+    ret = stabilisation_copter_init(&stabilisation_copter,
+                                    stabilisation_copter_default_config(),
+                                    &controls,
+                                    &ahrs,
+                                    &position_estimation,
+                                    &command.torque,
+                                    &command.thrust);
+    print_util_dbg_init_msg("[STABILISATION COPTER]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init controls
+    // -------------------------------------------------------------------------
+    ret = stabilisation_init(&controls);
+    print_util_dbg_init_msg("[CONTROLS]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init hud
+    // -------------------------------------------------------------------------
+    ret = hud_telemetry_init(&hud_structure,
+                             &position_estimation,
+                             &controls,
+                             &ahrs);
+    print_util_dbg_init_msg("[HUD]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+
+    // -------------------------------------------------------------------------
+    // Init servo mixing
+    // -------------------------------------------------------------------------
+    ret = servos_mix_quadcotper_diag_init(&servo_mix,
+                                          servos_mix_quadcopter_diag_default_config(),
+                                          &command.torque,
+                                          &command.thrust,
+                                          &servo_0,
+                                          &servo_1,
+                                          &servo_2,
+                                          &servo_3);
+    print_util_dbg_init_msg("[SERVOS MIX]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+    // -------------------------------------------------------------------------
+    // Init servo telemetry
+    // -------------------------------------------------------------------------
+    servos_telemetry_init(&servos_telemetry,
+                          &servo_0,
+                          &servo_1,
+                          &servo_2,
+                          &servo_3);
+
+    // -------------------------------------------------------------------------
+    // Init manual control
+    // -------------------------------------------------------------------------
+    ret = manual_control_init(&manual_control,
+                              &satellite,
+                              manual_control_default_config(),
+                              remote_default_config());
+    print_util_dbg_init_msg("[MANUAL CTRL]", ret);
+    init_success &= ret;
+    time_keeper_delay_ms(50);
+
+    // -------------------------------------------------------------------------
+    // Init data logging
+    // -------------------------------------------------------------------------
+    ret = toggle_logging_init(&toggle_logging,
+                              toggle_logging_default_config(),
+                              &state);
+
+    print_util_dbg_init_msg("[TOGGLE LOGGING]", ret);
+    init_success &= ret;
+
+    //--------------------------------------------------------------------------
+    // Init attitude controller
+    //--------------------------------------------------------------------------
+    attitude_controller_init(&attitude_controller,
+                             attitude_controller_default_config(),
+                             &ahrs,
+                             &command.attitude,
+                             &command.rate,
+                             &command.torque);
+
+    //--------------------------------------------------------------------------
+    // Init velocity controller
+    //--------------------------------------------------------------------------
+    velocity_controller_copter_conf_t velocity_controller_copter_config = velocity_controller_copter_default_config();
+    velocity_controller_copter_init(&velocity_controller,
+                                    velocity_controller_copter_config,
+                                    &ahrs,
+                                    &position_estimation,
+                                    &command.velocity,
+                                    &command.attitude,
+                                    &command.thrust);
+
+    //--------------------------------------------------------------------------
+    // Init vector field navigation
+    //--------------------------------------------------------------------------
+    vector_field_waypoint_conf_t vector_field_config;
+    vector_field_waypoint_init(&vector_field_waypoint,
+                               &vector_field_config,
+                               &waypoint_handler,
+                               &position_estimation,
+                               &command.velocity);
+
+    print_util_dbg_sep('-');
+    time_keeper_delay_ms(50);
+    print_util_dbg_init_msg("[CENTRAL_DATA]", init_success);
+    time_keeper_delay_ms(50);
+    print_util_dbg_sep('-');
+    time_keeper_delay_ms(50);
+
+    return init_success;
+}
---
+++ sample_projects/LEWing/tasks.cpp
@@ -0,0 +1,280 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2016, MAV'RIC Development Team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * \file tasks.c
+ *
+ * \author MAV'RIC Team
+ *
+ * \brief Definition of the tasks executed on the autopilot
+ *
+ ******************************************************************************/
+
+
+#include "sample_projects/LEQuad/tasks.hpp"
+#include "sample_projects/LEQuad/central_data.hpp"
+#include "communication/data_logging.hpp"
+
+
+void tasks_run_imu_update(Central_data* central_data)
+{
+    central_data->imu.update();
+    qfilter_update(&central_data->attitude_filter);
+    position_estimation_update(&central_data->position_estimation);
+}
+
+bool tasks_run_stabilisation(Central_data* central_data)
+{
+    tasks_run_imu_update(central_data);
+
+    mav_mode_t mode = central_data->state.mav_mode;
+
+    if (mav_modes_is_armed(mode))
+    {
+        if (mav_modes_is_auto(mode))
+        {
+            central_data->controls = central_data->controls_nav;
+            central_data->controls.control_mode = VELOCITY_COMMAND_MODE;
+
+            // if no waypoints are set, we do position hold therefore the yaw mode is absolute
+            if (((central_data->state.nav_plan_active && (central_data->navigation.internal_state == NAV_NAVIGATING)) || (central_data->navigation.internal_state == NAV_STOP_THERE)) || ((central_data->state.mav_state == MAV_STATE_CRITICAL) && (central_data->navigation.critical_behavior == FLY_TO_HOME_WP)))
+            {
+                central_data->controls.yaw_mode = YAW_RELATIVE;
+            }
+            else
+            {
+                central_data->controls.yaw_mode = YAW_ABSOLUTE;
+            }
+
+            //if (central_data->state.in_the_air || central_data->navigation.auto_takeoff)
+            if (central_data->navigation.internal_state > NAV_ON_GND)
+            {
+                stabilisation_copter_cascade_stabilise(&central_data->stabilisation_copter);
+                servos_mix_quadcopter_diag_update(&central_data->servo_mix);
+            }
+        }
+        else if (mav_modes_is_guided(mode))
+        {
+            central_data->controls = central_data->controls_nav;
+            central_data->controls.control_mode = VELOCITY_COMMAND_MODE;
+
+            if ((central_data->state.mav_state == MAV_STATE_CRITICAL) && (central_data->navigation.critical_behavior == FLY_TO_HOME_WP))
+            {
+                central_data->controls.yaw_mode = YAW_RELATIVE;
+            }
+            else
+            {
+                central_data->controls.yaw_mode = YAW_ABSOLUTE;
+            }
+
+            //if (central_data->state.in_the_air || central_data->navigation.auto_takeoff)
+            if (central_data->navigation.internal_state > NAV_ON_GND)
+            {
+                stabilisation_copter_cascade_stabilise(&central_data->stabilisation_copter);
+                servos_mix_quadcopter_diag_update(&central_data->servo_mix);
+            }
+        }
+        else if (mav_modes_is_stabilise(mode))
+        {
+            manual_control_get_velocity_vector(&central_data->manual_control, &central_data->controls);
+
+            central_data->controls.control_mode = VELOCITY_COMMAND_MODE;
+            central_data->controls.yaw_mode = YAW_RELATIVE;
+
+            //if (central_data->state.in_the_air || central_data->navigation.auto_takeoff)
+            if (central_data->navigation.internal_state > NAV_ON_GND)
+            {
+                stabilisation_copter_cascade_stabilise(&central_data->stabilisation_copter);
+                servos_mix_quadcopter_diag_update(&central_data->servo_mix);
+            }
+        }
+        else if (mav_modes_is_manual(mode))
+        {
+            manual_control_get_control_command(&central_data->manual_control, &central_data->controls);
+
+            central_data->controls.control_mode = ATTITUDE_COMMAND_MODE;
+            central_data->controls.yaw_mode = YAW_RELATIVE;
+
+            stabilisation_copter_cascade_stabilise(&central_data->stabilisation_copter);
+            servos_mix_quadcopter_diag_update(&central_data->servo_mix);
+        }
+        else
+        {
+            central_data->servo_0.failsafe();
+            central_data->servo_1.failsafe();
+            central_data->servo_2.failsafe();
+            central_data->servo_3.failsafe();
+        }
+    }
+    else
+    {
+        central_data->servo_0.failsafe();
+        central_data->servo_1.failsafe();
+        central_data->servo_2.failsafe();
+        central_data->servo_3.failsafe();
+    }
+
+    return true;
+}
+
+
+bool tasks_run_stabilisation_quaternion(Central_data* central_data);
+bool tasks_run_stabilisation_quaternion(Central_data* central_data)
+{
+    tasks_run_imu_update(central_data);
+
+    mav_mode_t mode = central_data->state.mav_mode;
+
+    if (mav_modes_is_armed(mode) == false)
+    {
+        // Set command to current heading
+        central_data->command.attitude.rpy[2] = coord_conventions_quat_to_aero(central_data->ahrs.qe).rpy[2];
+        central_data->servo_0.failsafe();
+        central_data->servo_1.failsafe();
+        central_data->servo_2.failsafe();
+        central_data->servo_3.failsafe();
+    }
+    else if (mav_modes_is_auto(mode))
+    {
+        vector_field_waypoint_update(&central_data->vector_field_waypoint);
+        velocity_controller_copter_update(&central_data->velocity_controller);
+        attitude_controller_update(&central_data->attitude_controller);
+        servos_mix_quadcopter_diag_update(&central_data->servo_mix);
+    }
+    else if (mav_modes_is_manual(mode) && mav_modes_is_guided(mode))
+    {
+        manual_control_get_velocity_command(&central_data->manual_control, &central_data->command.velocity, 1.0f);
+        velocity_controller_copter_update(&central_data->velocity_controller);
+        attitude_controller_update(&central_data->attitude_controller);
+        servos_mix_quadcopter_diag_update(&central_data->servo_mix);
+    }
+    else if (mav_modes_is_manual(mode) && mav_modes_is_stabilise(mode))
+    {
+        manual_control_get_attitude_command(&central_data->manual_control, 0.02f, &central_data->command.attitude, 1.0f);
+        manual_control_get_thrust_command(&central_data->manual_control, &central_data->command.thrust);
+        attitude_controller_update(&central_data->attitude_controller);
+
+        servos_mix_quadcopter_diag_update(&central_data->servo_mix);
+    }
+    else
+    {
+        central_data->servo_0.failsafe();
+        central_data->servo_1.failsafe();
+        central_data->servo_2.failsafe();
+        central_data->servo_3.failsafe();
+    }
+
+    return true;
+}
+
+
+bool tasks_run_gps_update(Central_data* central_data)
+{
+    central_data->gps.update();
+
+    return true;
+}
+
+
+bool tasks_run_barometer_update(Central_data* central_data)
+{
+
+    central_data->barometer.update();
+
+    return true;
+}
+
+
+bool tasks_run_sonar_update(Central_data* central_data)
+{
+    central_data->sonar.update();
+
+    return true;
+}
+
+
+bool tasks_sleep(Central_data* central_data)
+{
+    time_keeper_sleep_us(5000);
+    return true;
+}
+
+
+bool tasks_led_toggle(Led* led)
+{
+    led->toggle();
+
+    return true;
+}
+
+bool tasks_data_logging_update(Central_data* central_data)
+{
+    bool run_success = true;
+
+    run_success &= central_data->data_logging.update();
+    run_success &= central_data->data_logging2.update();
+
+    return run_success;
+}
+
+bool tasks_create_tasks(Central_data* central_data)
+{
+    bool init_success = true;
+
+    scheduler_t* scheduler = &central_data->scheduler;
+
+    init_success &= scheduler_add_task(scheduler, 4000,     RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_HIGHEST, (task_function_t)&tasks_run_stabilisation                         , (task_argument_t)central_data                         , 0);
+    // init_success &= scheduler_add_task(scheduler, 4000,      RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_HIGHEST, (task_function_t)&tasks_run_stabilisation_quaternion              , (task_argument_t)central_data                         , 0);
+
+    init_success &= scheduler_add_task(scheduler, 15000,    RUN_REGULAR, PERIODIC_RELATIVE, PRIORITY_HIGH   , (task_function_t)&tasks_run_barometer_update                      , (task_argument_t)central_data                     , 2);
+    init_success &= scheduler_add_task(scheduler, 100000,   RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_HIGH   , (task_function_t)&tasks_run_gps_update                            , (task_argument_t)central_data                     , 3);
+
+    init_success &= scheduler_add_task(scheduler, 10000,    RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_HIGH   , (task_function_t)&navigation_update                               , (task_argument_t)&central_data->navigation            , 5);
+    init_success &= scheduler_add_task(scheduler, 10000,    RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_HIGH   , (task_function_t)&waypoint_handler_update                         , (task_argument_t)&central_data->waypoint_handler      , 6);
+
+    init_success &= scheduler_add_task(scheduler, 200000,   RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_NORMAL , (task_function_t)&state_machine_update                            , (task_argument_t)&central_data->state_machine         , 7);
+
+    init_success &= scheduler_add_task(scheduler, 4000,     RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_NORMAL , (task_function_t)&mavlink_communication_update                    , (task_argument_t)&central_data->mavlink_communication , 8);
+
+    init_success &= scheduler_add_task(scheduler, 20000,    RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_HIGH , (task_function_t)&remote_update                                     , (task_argument_t)&central_data->manual_control.remote , 10);
+
+    init_success &= scheduler_add_task(scheduler, 500000,   RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_LOW    , (task_function_t)&tasks_run_sonar_update                          , (task_argument_t)central_data                         , 13);
+
+    init_success &= scheduler_add_task(scheduler, 500000,   RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_LOW    , (task_function_t)&tasks_led_toggle                                , (task_argument_t)&central_data->led                   , 1);
+
+    // init_success &= scheduler_add_task(scheduler, 4000,      RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_LOWEST , (task_function_t)&tasks_sleep                                     , (task_argument_t)central_data                         , 14);
+
+    init_success &= scheduler_add_task(scheduler, 10000,    RUN_REGULAR, PERIODIC_ABSOLUTE, PRIORITY_NORMAL , (task_function_t)&tasks_data_logging_update                       , (task_argument_t)central_data                         , 11);
+
+    scheduler_sort_tasks(scheduler);
+
+    return init_success;
+}
