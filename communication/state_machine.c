7a4b1dd483777118ddd112dc39596a8c613e28d7
ndousse <nicolas.dousse@epfl.ch>
2016-02-15 15:44:41 +0100

    Custom mode reset at arming

--- communication/state_machine.c
+++ communication/state_machine.c
@@ -193,6 +193,8 @@ task_return_t state_machine_update(state_machine_t* state_machine)
 			{
 				print_util_dbg_print("Switching from state_machine.\r\n");
 				state_switch_to_active_mode(state_machine->state, &state_new);
+
+				mode_custom_new = CUSTOM_BASE_MODE;
 			}
 			break;
 		
