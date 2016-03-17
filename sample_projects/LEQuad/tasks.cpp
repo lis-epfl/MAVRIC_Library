ccc51b8135af051223285453c0221afd5ade72c6
ndousse <nicolas.dousse@epfl.ch>
2016-03-17 19:32:08 +0100

    With the quad, if you perform DUBIN navigation type, the yaw_mode should be relative

--- sample_projects/LEQuad/tasks.cpp
+++ sample_projects/LEQuad/tasks.cpp
@@ -86,7 +86,7 @@ bool tasks_run_stabilisation(Central_data* central_data)
             central_data->controls = central_data->controls_nav;
             central_data->controls.control_mode = VELOCITY_COMMAND_MODE;
 
-            if ((central_data->state.mav_state == MAV_STATE_CRITICAL) && (central_data->navigation.critical_behavior == FLY_TO_HOME_WP))
+            if ( ((central_data->state.mav_state == MAV_STATE_CRITICAL) && (central_data->navigation.critical_behavior == FLY_TO_HOME_WP)) || (central_data->navigation.navigation_type == DUBIN))
             {
                 central_data->controls.yaw_mode = YAW_RELATIVE;
             }
