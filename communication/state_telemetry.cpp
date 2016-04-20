fc538db4f3075f5c1a5c5a5bab69d75d44a8471b
Basil Huber <basil.huber@gmail.com>
2016-04-20 16:21:55 +0200

    FIX: fixed error in state_telemetry_init which caused seg fault when arming from swarm control widget

--- communication/state_telemetry.cpp
+++ communication/state_telemetry.cpp
@@ -205,7 +205,7 @@ bool state_telemetry_init(State* state, Mavlink_message_handler* message_handler
     callbackcmd.compid_filter = MAV_COMP_ID_ALL;
     callbackcmd.compid_target = MAV_COMP_ID_ALL; // 0
     callbackcmd.function = (Mavlink_message_handler::cmd_callback_func_t)         &state_telemetry_set_mode_from_cmd;
-    callback.module_struct  = (Mavlink_message_handler::handling_module_struct_t) state;
+    callbackcmd.module_struct  = (Mavlink_message_handler::handling_module_struct_t) state;
     init_success &= message_handler->add_cmd_callback(&callbackcmd);
 
     callbackcmd.command_id = MAV_CMD_COMPONENT_ARM_DISARM; // 400
