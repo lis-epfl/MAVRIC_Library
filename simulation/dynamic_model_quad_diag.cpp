6a9d69d4682ccb3f6719eabae0491ad3b35306da
Julien Lecoeur <julien.lecoeur@epfl.ch>
2016-04-29 16:40:49 +0200

    Sim: modify effect of ground to have 1G acceleration

--- simulation/dynamic_model_quad_diag.cpp
+++ simulation/dynamic_model_quad_diag.cpp
@@ -142,11 +142,17 @@ bool Dynamic_model_quad_diag::update(void)
 
     // velocity and position integration
 
-    // check altitude - if it is lower than 0, clamp everything (this is in NED, assuming negative altitude)
-    if (local_position_.pos[Z] > 0)
+    // check altitude - if it is lower than ground, clamp everything (this is in NED, assuming negative altitude)
+    if (local_position_.pos[Z] > -0.05)
     {
         vel_[Z] = 0.0f;
         local_position_.pos[Z] = 0.0f;
+
+        // Add resistive force towards ground, proportionnal to distance to ground
+        for (i = 0; i < 3; i++)
+        {
+            lin_forces_bf_[i] += up_vec.v[i] * config_.total_mass * config_.gravity + local_position_.pos[Z] * 10.0f;
+        }
 
         // slow down... (will make velocity slightly inconsistent until next update cycle, but shouldn't matter much)
         for (i = 0; i < 3; i++)
