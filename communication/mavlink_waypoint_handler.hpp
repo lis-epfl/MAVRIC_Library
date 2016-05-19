2d84dbcd1d10dec3274ffbcdd31f822fd17596c3
Matt Douglas <mdouglas90@gmail.com>
2016-05-19 12:21:24 +0200

    Made some public variables protected

--- communication/mavlink_waypoint_handler.hpp
+++ communication/mavlink_waypoint_handler.hpp
@@ -122,11 +122,9 @@ public:
     static inline conf_t default_config();
 
 
-    waypoint_local_struct_t waypoint_hold_coordinates;           ///< The coordinates of the waypoint in position hold mode (MAV_MODE_GUIDED_ARMED)
-
+protected:
     waypoint_struct_t waypoint_list[MAX_WAYPOINTS];              ///< The array of all waypoints (max MAX_WAYPOINTS)
 
-protected:
     uint16_t waypoint_count_;                                    ///< The total number of waypoints
     int8_t current_waypoint_index_;                              ///< The number of the current waypoint
     bool hold_waypoint_set_;                                     ///< Flag to tell if the hold position waypoint is set
