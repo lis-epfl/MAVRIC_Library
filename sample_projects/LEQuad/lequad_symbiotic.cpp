706f8e5259e7c514e25bbfc6c149e9effbe96877
Alexandre <alexandre.cherpillod@epfl.ch>
2016-06-07 09:21:23 +0200

    imp

--- sample_projects/LEQuad/lequad_symbiotic.cpp
+++ sample_projects/LEQuad/lequad_symbiotic.cpp
@@ -101,6 +101,15 @@ bool LEQuad_symbiotic::main_task(void)
                 controls = controls_nav;
                 controls.control_mode = VELOCITY_COMMAND_MODE;
 
+                //if still on ground, don't move and wait on takeoff
+				if(navigation.internal_state_ == Navigation::NAV_ON_GND)
+				{
+					controls.rpy[ROLL] = 0.0f;
+					controls.rpy[PITCH] = 0.0f;
+					controls.thrust = 0.0f;
+					controls.control_mode = ATTITUDE_COMMAND_MODE;
+				}
+
                 // if no waypoints are set, we do position hold therefore the yaw mode is absolute
                 if ((((state.nav_plan_active || (navigation.navigation_strategy == Navigation::strategy_t::DUBIN)) && (navigation.internal_state_ == Navigation::NAV_NAVIGATING)) || (navigation.internal_state_ == Navigation::NAV_STOP_THERE))
               	   || ((state.mav_state_ == MAV_STATE_CRITICAL) && (navigation.critical_behavior == Navigation::FLY_TO_HOME_WP)))
