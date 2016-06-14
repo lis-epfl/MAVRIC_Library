f87135f1ddfb841ee61cd6635437d0dc250753b0
Julien Lecoeur <julien.lecoeur@epfl.ch>
2016-06-14 09:40:05 +0200

    Missing file

---
+++ sensing/ahrs.hpp
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2016, MAV'RIC Development Team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * \file ahrs.hpp
+ *
+ * \author MAV'RIC Team
+ * \author Gregoire Heitz
+ *
+ * \brief This file implements data structure for attitude estimate
+ *
+ ******************************************************************************/
+
+
+#ifndef AHRS_HPP_
+#define AHRS_HPP_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "util/quaternions.h"
+
+
+/**
+ * \brief The calibration level of the filter
+ */
+typedef enum
+{
+    AHRS_INITIALISING   = 0,    ///< Calibration level: Not initialised
+    AHRS_LEVELING       = 1,    ///< Calibration level: No calibration, attitude estimation correction by accelero/magneto measurements
+    AHRS_CONVERGING     = 2,    ///< Calibration level: leveling, correction of gyro biais
+    AHRS_READY          = 3,    ///< Calibration level: leveled
+} ahrs_state_t;
+
+
+/**
+ * \brief Structure containing the Attitude and Heading Reference System
+ */
+typedef struct
+{
+    quat_t  qe;                         ///< quaternion defining the Attitude estimation of the platform
+
+    float   angular_speed[3];           ///< Gyro rates
+    float   linear_acc[3];              ///< Acceleration WITHOUT gravity
+
+    ahrs_state_t internal_state;        ///< Leveling state of the ahrs
+    float        last_update_s;         ///< The time of the last IMU update in s
+} ahrs_t;
+
+
+/**
+ * \brief   Initialiases the ahrs structure
+ *
+ * \param   ahrs                Pointer to ahrs structure
+ *
+ * \return  True if the init succeed, false otherwise
+ */
+bool ahrs_init(ahrs_t* ahrs);
+
+
+/**
+ * \brief   Returns an initialized ahrs_t
+ *
+ * \detail  Used to initialize ahrs in initalizer list
+ *
+ * \return  Initialized ahrs_t
+ */
+ahrs_t ahrs_initialized(void);
+
+
+#endif /* AHRS_HPP_ */
