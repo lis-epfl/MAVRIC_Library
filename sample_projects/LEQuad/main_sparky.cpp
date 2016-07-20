b9eac129205e39c88a6fbb9bfd9150151bd63c48
Julien Lecoeur <julien.lecoeur@epfl.ch>
2016-07-20 22:34:20 +0200

    LED blinking example on board sparky 2.0

---
+++ sample_projects/LEQuad/main_sparky.cpp
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ * Copyright (c) 2009-2016, MAV'RIC Development Team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * \file main.cpp
+ *
+ * \author MAV'RIC Team
+ *
+ * \brief Main file
+ *
+ ******************************************************************************/
+
+#include "boards/mavrimini.hpp"
+#include "sample_projects/LEQuad/lequad.hpp"
+#include "hal/common/time_keeper.hpp"
+
+extern "C"
+{
+#include "util/print_util.h"
+}
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+int main(int argc, char** argv)
+{
+    // #############################################################################################
+    // #############  Clock Setup ##################################################################
+    // #############################################################################################
+    // Set STM32 to 168 MHz
+    rcc_clock_setup_hse_3v3(&hse_25mhz_3v3[CLOCK_3V3_168MHZ]);
+
+    // Enable GPIO clock
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_periph_clock_enable(RCC_GPIOC);
+    rcc_periph_clock_enable(RCC_GPIOD);
+
+    time_keeper_init();
+
+    // #############################################################################################
+    // #############  LED Setup ##################################################################
+    // #############################################################################################
+    gpio_stm32_conf_t led_err_gpio_config;
+    gpio_stm32_conf_t led_stat_gpio_config;
+    gpio_stm32_conf_t led_rf_gpio_config;
+
+    led_err_gpio_config.port      = GPIO_STM32_PORT_B;
+    led_err_gpio_config.pin       = GPIO_STM32_PIN_4;
+    led_err_gpio_config.dir      = GPIO_OUTPUT;
+    led_err_gpio_config.pull     = GPIO_PULL_UPDOWN_NONE;
+
+    led_stat_gpio_config.port     = GPIO_STM32_PORT_B;
+    led_stat_gpio_config.pin      = GPIO_STM32_PIN_5;
+    led_stat_gpio_config.dir      = GPIO_OUTPUT;
+    led_stat_gpio_config.pull     = GPIO_PULL_UPDOWN_NONE;
+    led_rf_gpio_config.port    = GPIO_STM32_PORT_B;
+    led_rf_gpio_config.pin     = GPIO_STM32_PIN_6;
+    led_rf_gpio_config.dir      = GPIO_OUTPUT;
+    led_rf_gpio_config.pull     = GPIO_PULL_UPDOWN_NONE;
+
+    Gpio_stm32 led_err_gpio(led_err_gpio_config);
+    Gpio_stm32 led_stat_gpio(led_stat_gpio_config);
+    Gpio_stm32 led_rf_gpio(led_rf_gpio_config);
+    Led_gpio led_err(led_err_gpio);
+    Led_gpio led_stat(led_stat_gpio);
+    Led_gpio led_rf(led_rf_gpio);
+
+    led_err_gpio.init();
+    led_stat_gpio.init();
+    led_rf_gpio.init();
+
+    // #############################################################################################
+    // #############  Blink ! ######################################################################
+    // #############################################################################################
+    led_err.off();
+    led_stat.off();
+    led_rf.off();
+
+    while (1)
+    {
+        time_keeper_delay_ms(100);
+        led_rf.toggle();
+        time_keeper_delay_ms(100);
+        led_stat.toggle();
+        time_keeper_delay_ms(100);
+        led_err.toggle();
+    }
+
+    return 0;
+}
