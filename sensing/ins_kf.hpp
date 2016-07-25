615a40edd6ddffe681ca2fbf7270e40207408af2
Julien Lecoeur <julien.lecoeur@epfl.ch>
2016-07-25 10:52:34 +0200

    Missing doc

--- sensing/ins_kf.hpp
+++ sensing/ins_kf.hpp
@@ -194,18 +194,18 @@ private:
 
     conf_t config_;                      ///< Configuration
 
-    std::array<float,3> pos_;
-    std::array<float,3> vel_;
-    float absolute_altitude_;
+    std::array<float,3> pos_;           ///< Estimated position (NED)
+    std::array<float,3> vel_;           ///< Estimated velocity (NED)
+    float absolute_altitude_;           ///< Estimated altitude above sea level
 
-    Mat<3,8> H_gpsvel_;
-    Mat<3,3> R_gpsvel_;
-    Mat<1,8> H_baro_;
-    Mat<1,1> R_baro_;
-    Mat<1,8> H_sonar_;
-    Mat<1,1> R_sonar_;
-    Mat<3,8> H_flow_;
-    Mat<3,3> R_flow_;
+    Mat<3,8> H_gpsvel_;                 ///< Measurement matrix for GPS velocity
+    Mat<3,3> R_gpsvel_;                 ///< Noise matrix for GPS velocity
+    Mat<1,8> H_baro_;                   ///< Measurement matrix for barometer
+    Mat<1,1> R_baro_;                   ///< Noise matrix for barometer
+    Mat<1,8> H_sonar_;                  ///< Measurement matrix for sonar
+    Mat<1,1> R_sonar_;                  ///< Noise matrix for sonar
+    Mat<3,8> H_flow_;                   ///< Measurement matrix for optic flow
+    Mat<3,3> R_flow_;                   ///< Noise matrix for optic flow
 
     float last_accel_update_s_;          ///< Last time we updated the estimate using accelerometer
     float last_sonar_update_s_;          ///< Last time we updated the estimate using sonar
