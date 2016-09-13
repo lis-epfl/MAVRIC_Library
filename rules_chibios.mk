52c933f564875f068b4c38168604e9406bd3fdcf
Julien Lecoeur <julien.lecoeur@epfl.ch>
2016-09-13 18:22:32 +0200

    Clean rules_chibios.mk

--- rules_chibios.mk
+++ rules_chibios.mk
@@ -1,7 +1,6 @@
 CFLAGS   += -D__MAVRIC_ENDIAN_LITTLE__
 CXXFLAGS += -D__MAVRIC_ENDIAN_LITTLE__
 
-
 LIB_SRCS += hal/chibios/gpio_chibios.cpp
 LIB_SRCS += hal/chibios/i2c_chibios.cpp
 LIB_SRCS += hal/chibios/pwm_chibios.cpp
@@ -9,20 +8,3 @@ LIB_SRCS += hal/chibios/serial_chibios.cpp
 LIB_SRCS += hal/chibios/spi_chibios.cpp
 LIB_SRCS += hal/chibios/stubs.cpp
 LIB_SRCS += hal/chibios/time_keeper.cpp
-
-# CSRC += hal/chibios/usbcfg.c
-
-
-# LIB_SRCS += boards/mavrimini.cpp
-# LIB_SRCS += boards/sparky_v2.cpp
-#
-# LIB_SRCS += drivers/state_display_mavrimini.cpp
-# LIB_SRCS += drivers/state_display_sparky_v2.cpp
-#
-# LIB_SRCS += hal/stm32/time_keeper.c
-# LIB_SRCS += hal/stm32/gpio_stm32.cpp
-# LIB_SRCS += hal/stm32/serial_stm32.cpp
-# LIB_SRCS += hal/stm32/serial_usb_stm32.cpp
-# LIB_SRCS += hal/stm32/spi_stm32.cpp
-# LIB_SRCS += hal/stm32/i2c_stm32.cpp
-# LIB_SRCS += hal/stm32/pwm_stm32.cpp
