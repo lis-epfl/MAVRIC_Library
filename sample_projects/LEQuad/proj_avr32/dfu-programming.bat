0627fbc853e1e0fa0a2f31c522f626198898e657
Anand B <banand@live.com>
2016-09-27 14:46:28 +0200

    - Runtime files.. Delete later

---
+++ sample_projects/LEQuad/proj_avr32/dfu-programming.bat
@@ -0,0 +1,5 @@
+..\..\..\..\Mavric\MAVRIC\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 erase
+..\..\..\..\Mavric\MAVRIC\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 get
+..\..\..\..\Mavric\MAVRIC\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 flash LEQuadAV.hex --suppress-bootloader-mem 
+..\..\..\..\Mavric\MAVRIC\Utilities\DFU-programmer\DFU\dfu-programmer at32uc3c1512 reset
+pause
