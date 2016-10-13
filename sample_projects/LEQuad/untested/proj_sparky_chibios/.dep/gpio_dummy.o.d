dbb5bbb1c4ddc76551aff62afcf63854cb5a7690
Julien Lecoeur <jlecoeur@users.noreply.github.com>
2016-10-13 10:18:33 +0200

    Quickfix/dep folder (#338)
    * Remove .dep folder from git
    
    * Add .dep to gitignore

--- sample_projects/LEQuad/untested/proj_sparky_chibios/.dep/gpio_dummy.o.d
+++
@@ -1,6 +0,0 @@
-build/obj/gpio_dummy.o: ../../../..//hal/dummy/gpio_dummy.cpp \
- ../../../../hal/dummy/gpio_dummy.hpp ../../../../hal/common/gpio.hpp
-
-../../../../hal/dummy/gpio_dummy.hpp:
-
-../../../../hal/common/gpio.hpp:
