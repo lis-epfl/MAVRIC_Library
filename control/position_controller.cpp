0944b368cc1fac0f7e33df2986c01cdffcc7cf13
Julien Lecoeur <julien.lecoeur@epfl.ch>
2016-11-30 18:35:52 +0100

    Add cruise speed and cruise dist to distance controller with ramp up and down accelerations

--- control/position_controller.cpp
+++ control/position_controller.cpp
@@ -48,9 +48,10 @@
 Position_controller::Position_controller(args_t args, const conf_t& config) :
     ins_(args.ins),
     ahrs_(args.ahrs),
-    position_command_{std::array<float,3>{{0.0f, 0.0f, 0.0f}}, 0.0f},
-    velocity_command_{std::array<float,3>{{0.0f, 0.0f, 0.0f}}, 0.0f},
-    ctrl_mode_(ctrl_mode_t::POS_XYZ),
+    position_command_(position_command_t{{{0.0f, 0.0f, 0.0f}}, 0.0f}),
+    velocity_command_(velocity_command_t{{{0.0f, 0.0f, 0.0f}}, 0.0f}),
+    cruise_speed_(config.cruise_speed),
+    cruise_dist_(config.cruise_dist),
     max_climb_rate_(config.max_climb_rate),
     kp_yaw_(config.kp_yaw)
 {
@@ -59,21 +60,15 @@ Position_controller::Position_controller(args_t args, const conf_t& config) :
 
 bool Position_controller::update()
 {
-    /* get current vehicle position in local frame */
+    // get current vehicle position in local frame
     local_position_t local_pos = ins_.position_lf();
+    float v_current = vectors_norm(ins_.velocity_lf().data());
 
     // get position relative to the target position (0 in z dir if z is controlled in velocity)
     float rel_goal_pos[3];
     rel_goal_pos[X] = position_command_.xyz[X] - local_pos[X];
     rel_goal_pos[Y] = position_command_.xyz[Y] - local_pos[Y];
-    if (ctrl_mode_ == ctrl_mode_t::POS_XY_VEL_Z)
-    {
-        rel_goal_pos[Z] = 0.0f;
-    }
-    else
-    {
-        rel_goal_pos[Z] = position_command_.xyz[Z] - local_pos[Z];
-    }
+    rel_goal_pos[Z] = position_command_.xyz[Z] - local_pos[Z];
 
     // Normalize relative goal position to get desired direction (unit vector)
     float goal_distance = vectors_norm(rel_goal_pos);
@@ -84,10 +79,22 @@ bool Position_controller::update()
     goal_dir[Z] = rel_goal_pos[Z] * scale;
 
     // get desired speed from pid controller
-    float v_desired = pid_controller_update(&pid_controller_, goal_distance);
+    float pid_dist     = SQR(cruise_speed_) / (SQR(pid_controller_.p_gain) * cruise_dist_);
+    float v_desired;
+    if (goal_distance > pid_dist)
+    {
+        v_desired = maths_clip(cruise_speed_ * maths_fast_sqrt(goal_distance / cruise_dist_), cruise_speed_);
+
+        // do ramp-up or ramp-down
+        v_desired = v_current + maths_clip(v_desired - v_current, cruise_speed_ / cruise_dist_);
+    }
+    else
+    {
+        v_desired = pid_controller_update(&pid_controller_, goal_distance);
+    }
 
     // make sure maximal climb rate is not exceeded, reduce v_desired if necessary
-    if (v_desired *  maths_f_abs(goal_dir[Z]) > max_climb_rate_)
+    if (v_desired * maths_f_abs(goal_dir[Z]) > max_climb_rate_)
     {
         v_desired = max_climb_rate_ / maths_f_abs(goal_dir[Z]);
     }
@@ -95,7 +102,7 @@ bool Position_controller::update()
     // Compute desired velocity command
     velocity_command_.xyz[X]  = goal_dir[X] * v_desired;
     velocity_command_.xyz[Y]  = goal_dir[Y] * v_desired;
-    velocity_command_.xyz[Z]  = ctrl_mode_ == ctrl_mode_t::POS_XY_VEL_Z ? zvel_command_ : goal_dir[Z] * v_desired;
+    velocity_command_.xyz[Z]  = goal_dir[Z] * v_desired;
     velocity_command_.heading = position_command_.heading;
 
     // Reduce XY velocity according to yaw error: this gives time to align to next waypoint before accelerating
@@ -103,7 +110,6 @@ bool Position_controller::update()
     float yaw_error_factor = 1.0f - maths_f_abs(maths_clip(maths_calc_smaller_angle(velocity_command_.heading - yaw_current), 1.0f));
     velocity_command_.xyz[X] *= yaw_error_factor;
     velocity_command_.xyz[Y] *= yaw_error_factor;
-
 
 	return true;
 }
